fc7b8871e4c6fa88b404b08bf111c7b4dd2c690b
isabelpm <isabelpuig90@gmail.com>
2021-04-22 15:49:37 +0200

    añadiddo texto "copyright" en el footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,8 @@ import '../components/footer.css';
 const Footer = () => {
     return (
         <Container fluid className="p-0">
-            <ul expand="lg" className="App-footer p-2 justify-text-center">
-                <li > / • By Isabel PM • / </li>
+            <ul expand="lg" className="App-footer p-2 text-align-center">
+                <li > // • copyright 2021 by Isabel PM • // </li>
             </ul>
         </Container>
     )
